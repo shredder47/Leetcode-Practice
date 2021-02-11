f2083aa1e12b993a8bdbed46ced8f0fd930c82bc
Abhishek Sengupta <abhishek.sengupta047@gmail.com>
2021-02-11 11:32:37 +0530

    https://leetcode.com/problems/pascals-triangle

---
+++ src/com/shredder/array/PascalsTriangle.java
@@ -0,0 +1,67 @@
+package com.shredder.array;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+/*
+ *  Input: 5
+    Output:
+    [
+         [1],
+        [1,1],
+       [1,2,1],
+      [1,3,3,1],
+     [1,4,6,4,1]
+    ]
+ */
+public class PascalsTriangle {
+
+    public static void main(String[] args) {
+        System.out.println(generate(5));
+
+    }
+
+    public static List<List<Integer>> generate(int numRows) {
+
+        if(numRows == 0) return Collections.emptyList();
+
+        List<List<Integer>> outPutArr = new ArrayList<>();
+        outPutArr.add(Collections.singletonList(1)); // Initially adding one item
+
+        //Staring from 2 ,as first item is already added
+        int counter = 2;
+        while (counter <= numRows){
+
+            // Getting the copy of last Item (i.e list of Integers) and adding zero at first and last place
+            List<Integer> lastList = new ArrayList<>(outPutArr.get(outPutArr.size() - 1));
+            lastList.add(0,0);
+            lastList.add(0);
+
+            //Temp Array to store the item to be added to the existing list
+            List<Integer> arrToAppend = new ArrayList<>();
+
+            //This ptr will advance till n - 1
+            int ptr = 0;
+
+            while (ptr < lastList.size() - 1) {
+                arrToAppend.add(
+                                    lastList.get(ptr) //Item at ptr
+                                            +
+                                    lastList.get(ptr + 1) // Next Adjustment item
+                               );
+                ptr++;
+            }
+            // Add result to final output arr
+            outPutArr.add(arrToAppend);
+
+            // Increment the step counter
+            counter++;
+        }
+
+        return outPutArr;
+    }
+
+
+}
