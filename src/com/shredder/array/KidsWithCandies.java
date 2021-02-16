e1c0c09fda6566b4588f9a0a22156a31aba1851e
Abhishek Sengupta <abhishek.sengupta047@gmail.com>
2021-02-16 11:51:00 +0530

    https://leetcode.com/problems/kids-with-the-greatest-number-of-candies

---
+++ src/com/shredder/array/KidsWithCandies.java
@@ -0,0 +1,30 @@
+package com.shredder.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KidsWithCandies {
+
+
+    public static void main(String[] args) {
+
+        System.out.println(kidsWithCandies(new int[]{12, 1, 12}, 10));
+    }
+
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = Integer.MIN_VALUE;
+
+        for (int candy : candies) {
+            max = Math.max(max, candy);
+        }
+        List<Boolean> resultArr = new ArrayList<>();
+        for (int candy : candies) {
+            if (candy + extraCandies >= max) {
+                resultArr.add(true);
+            } else {
+                resultArr.add(false);
+            }
+        }
+        return resultArr;
+    }
+}
